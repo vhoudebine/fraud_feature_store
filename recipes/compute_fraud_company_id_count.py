65a6bf3e37fe9c25a355f26ef29a5ffc3872f181
admin <admin>
2020-07-20 15:04:24 +0000

    Recipe FEATURE_STORE_FRAUD.compute_fraud_company_id_count saved through notebook notebook editor for compute_fraud_company_id_count

--- recipes/compute_fraud_company_id_count.py
+++ recipes/compute_fraud_company_id_count.py
@@ -1,20 +1,26 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
+from fraud_features import windows
 # Read recipe inputs
 transactions_repository_prepared = dataiku.Dataset("transactions_repository_prepared")
-transactions_repository_prepared_df = transactions_repository_prepared.get_dataframe()
+df = transactions_repository_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from fraud_features import windows
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.columns
 
-fraud_company_id_count_df = transactions_repository_prepared_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['count_frauds_company'] = windows.LaggedSum(df, 'id_company', 'date', 'fraude')
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df = df[['ref_id','id_company','count_frauds_company']]
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 fraud_company_id_count = dataiku.Dataset("fraud_company_id_count")
-fraud_company_id_count.write_with_schema(fraud_company_id_count_df)
+fraud_company_id_count.write_with_schema(df)
